d06905bd5ab54af2cd08a04d811516b61b6c3093
Koks, B.T. (Boye) <btkoks@student.ru.nl>
2017-10-02 16:58:53 +0200

    Added file with hive images

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import hextilestore as hts
 grid = hg.Grid(4)
 succeed = \
 grid.setTile(2,0,-2, "Queen", hts.black_queen) and \
-grid.setTile(1,-1,0, "Queen", hts.black_queen) and \
-grid.setTile(8,2,1, "Empty", hts.empty)
+grid.setTile(1,-1,0, "Queen", hts.black_queen)
 print(succeed)
 hv.HexWindow(grid, 50)
